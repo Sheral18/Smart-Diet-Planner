eada50f02bb499bb60cff982d193d1e7d6db08fc
Sheral Waskar <93430457+Sheral18@users.noreply.github.com>
2022-11-03 00:05:26 +0530

    Add files via upload
    commit

---
+++ app.py
@@ -0,0 +1,17 @@
+from logging import debug
+from website.views import views
+from flask import Flask
+import os
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'aman17'
+    return app
+
+app = create_app()
+
+app.register_blueprint(views)
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0",port=os.environ.get("PORT",5000))
+    # app.run(debug=True)
